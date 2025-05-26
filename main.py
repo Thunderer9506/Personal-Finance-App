61c312ecfe48b25e62bfe229fd42eda94b5587c1
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-05-26 16:21:28 +0530

    render the transaction history using for loop in html

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import redirect
 from datetime import datetime
 
 app = Flask(__name__)
-
+id = 1
 todaysDate = datetime.now().strftime("%d-%m-%Y")
 expenseCategory = ['Food','Transportation','Bill','Shoping','Education']
 incomeCategory = ['Salary','Awards','Coupons','Grants','Rental']
-transactionHistory = [1,'Income', 2500.00,'Food','Food and Dining','25-05-2025']
+transactionHistory = [{'id':1,'type':'Income','amount': 2500.00,'category':'Food','description':'Food and Dining','date':'25-05-2025'}]
 
 
 @app.context_processor
@@ -18,10 +19,8 @@ def inject_global():
 @app.route("/", methods=["GET","POST"])
 def home():
     if request.method == "POST":
-        print(request.form["amount"])
-        print(request.form["transactionType"])
-        print(request.form["categories"])
-        print(request.form["description"])
-
-    return render_template('index.html',category = {'expense': expenseCategory,'income':incomeCategory})
+        data = {'id':id,'type':request.form["transactionType"],'amount': float(request.form["amount"]),'category':request.form["categories"],'description':request.form["description"],'date':todaysDate}
+        id += 1
+        redirect('/')
+    return render_template('index.html',category = {'expense': expenseCategory,'income':incomeCategory},history=transactionHistory)
 
