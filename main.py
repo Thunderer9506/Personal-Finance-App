93b17acd207182b8fcf845a968c05b3efd24e7df
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-05-26 17:37:48 +0530

    give amount in the template, fixed resubmission error

--- main.py
+++ main.py
@@ -9,8 +9,16 @@ id = 1
 todaysDate = datetime.now().strftime("%d-%m-%Y")
 expenseCategory = ['Food','Transportation','Bill','Shoping','Education']
 incomeCategory = ['Salary','Awards','Coupons','Grants','Rental']
-transactionHistory = [{'id':1,'type':'Income','amount': 2500.00,'category':'Food','description':'Food and Dining','date':'25-05-2025'}]
+transactionHistory = [{'id':1,'type':'Expense','amount': 2500.00,'category':'Food','description':'Food and Dining','date':'25-05-2025'}]
 
+def getAmount():
+    sum = 0
+    for i in transactionHistory:
+        if i['type'] == 'Income':
+            sum += i['amount']
+        elif i['type'] == 'Expense':
+            sum -= i['amount']
+    return sum
 
 @app.context_processor
 def inject_global():
@@ -23,6 +31,9 @@ def home():
         id += 1
         data = {'id':id,'type':request.form["transactionType"],'amount': float(request.form["amount"]),'category':request.form["categories"],'description':request.form["description"],'date':todaysDate}
         transactionHistory.append(data)
-        redirect('/')
-    return render_template('index.html',category = {'expense': expenseCategory,'income':incomeCategory},history=transactionHistory)
+        return redirect('/')
+    return render_template('index.html',
+                           category = {'expense': expenseCategory,'income':incomeCategory},
+                           history=transactionHistory,
+                           amount = getAmount())
 
