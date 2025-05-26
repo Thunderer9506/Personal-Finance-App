e87c90d40d1c8dff7c96bc1c0c6d72614cf38803
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-05-26 16:12:38 +0530

    making the date variable global in templates

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from datetime import datetime
 
 app = Flask(__name__)
 
+todaysDate = datetime.now().strftime("%d-%m-%Y")
 expenseCategory = ['Food','Transportation','Bill','Shoping','Education']
 incomeCategory = ['Salary','Awards','Coupons','Grants','Rental']
+transactionHistory = [1,'Income', 2500.00,'Food','Food and Dining','25-05-2025']
 
+
+@app.context_processor
+def inject_global():
+    return {'date': todaysDate}
 
 @app.route("/", methods=["GET","POST"])
 def home():
