6e5e56a5f2ef0a71964c1d34aaafd987aaa48e4f
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-11-11 20:57:05 +0530

    testing new table structure

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
-from sqlalchemy import String, Integer, Text,create_engine, Date, ForeignKey, select
+from sqlalchemy import String, Integer, Text,create_engine, Date, ForeignKey, select, insert, and_
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship, sessionmaker
 from datetime import date,datetime
 from extensions import db
 from typing import List
 import uuid
+from argon2 import PasswordHasher
 
 # ---------- Engine ----------
 # engine = create_engine("sqlite:///./instance/finance.db")
@@ -18,23 +19,23 @@ import uuid
 # ---------- Models ----------
 class User(db.Model):
     __tablename__ = "users"
-    id: Mapped[int] = mapped_column(primary_key=True,autoincrement=False,default=uuid.uuid4())
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
     email:Mapped[str] = mapped_column(String,nullable=False)
     password: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     transactions: Mapped[List["Transaction"]] = relationship(back_populates="user")
 
     def __repr__(self):
-        return f"<User: Id:{self.id}, Full Name:{self.fullName}, Phone Number:{self.phoneNumber}>"
+        return f"<User: Id:{self.id}, Email:{self.email}, Password:{self.password}>"
 
 class Transaction(db.Model):
     __tablename__ = "transactions"
-    id: Mapped[int] = mapped_column(primary_key=True,autoincrement=False,default=uuid.uuid1())
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
     type: Mapped[str] = mapped_column(String,nullable=False)
     amount: Mapped[int] = mapped_column(Integer,nullable=False)
     category: Mapped[str] = mapped_column(String,nullable=False)
     description: Mapped[str] = mapped_column(Text, default="No Description")
     date: Mapped[date] = mapped_column(Date,nullable=False, default=date.today())
-    userId: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    userId: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id"))
     user: Mapped["User"] = relationship(back_populates="transactions")
 
     def __repr__(self):
@@ -44,23 +45,26 @@ class Transaction(db.Model):
 # if __name__ == "__main__":
     # Base.metadata.create_all(engine)
     # print("--- Tables has been created ---")
+    # ph = PasswordHasher()
+    # Session = sessionmaker(bind=engine)
 
     # --- Reading data from user and transaction ---
-    # Session = sessionmaker(bind=engine)
     # with Session() as session:
-    #     stmt = select(User).where(User.id == 1)
+    #     stmt = select(User).where(and_(User.email == , User.password ==))
     #     user = session.scalars(stmt).first()
     #     transactions = user.transactions
     #     first_transactions = transactions[0]
     #     print(first_transactions.user)
 
     # --- Create Users ---
+    # fake_users = [
+    #     {"id":uuid.uuid4(),"email": "aarav.mehta@example.com", "password": ph.hash(password="Pass@123")},
+    #     {"id":uuid.uuid4(),"email": "isha.kapoor@example.com", "password": ph.hash(password="Qwerty!45")},
+    #     {"id":uuid.uuid4(),"email": "rohan.desai@example.com", "password": ph.hash(password="Secret#908")},
+    # ]
     # with Session() as session:
-    #     user1 = User(fullName = 'Aarav Mehta', phoneNumber = 9876543210)
-    #     user2 = User(fullName = 'Priya Sharma', phoneNumber = 9123456789)
-    #     user3 = User(fullName = 'Rohan Iyer', phoneNumber = 9988776655)
-    #     session.add_all([user1,user2,user3])
-    #     session.commit()
+        # session.execute(insert(User),fake_users)
+        # session.commit()
 
     # --- Create Transactions ---
     # with Session() as session:
@@ -82,8 +86,17 @@ class Transaction(db.Model):
     #             (15, 3, 'Income', 2000.00, 'Investment', 'Stock dividend payout', '2025-10-10')
     #         ]
         
-    #     for i in data:
-    #         transaction1 = Transaction(type=i[2],amount=i[3],category=i[4],description=i[5],date=datetime.strptime(i[6],"%Y-%m-%d"),userId=i[1])
-    #         session.add(transaction1)
-    #         session.commit()
-    
+    # for i in data:
+    #     if i[1] == 1:
+    #         stmt = select(User).where(User.email == "aarav.mehta@example.com")
+    #     elif i[1] == 2:
+    #         stmt = select(User).where(User.email == "isha.kapoor@example.com")
+    #     elif i[1] == 3:
+    #         stmt = select(User).where(User.email == "rohan.desai@example.com")
+
+    #     user = session.scalar(stmt)
+    #     transac = Transaction(id = uuid.uuid4(),type=i[2],amount=i[3],category=i[4],description=i[5],date=datetime.strptime(i[6],"%Y-%m-%d"),userId=user.id)
+    #     session.add(transac)
+    #     session.commit()
+
+        
